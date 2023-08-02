1807d9ae740229bae9bedead7a6de6560814a88f
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2023-08-02 23:43:33 +0530

    Updated Input Field Component

--- src/Components/Common/InputText.jsx
+++ src/Components/Common/InputText.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../../CSS/InputText.css';
 
 const InputText = (props) => {
-    const { labelName, id } = props;
+    const {label, onChange, name, type, errorMessage, ...inputProps} = props;
+    const [focused, setFocused] = useState(false);
+
+    const handleFocus = (e) => {
+        setFocused(true);        
+    }
 
     return (
         <div className='w-full'>
-            <div className='relative mt-4 w-full'>
-                <input type='text' id={id} name={id} className='peer h-12 w-full min-w-[200px] px-5 text-md bg-grey border border-[#ddd] rounded-lg placeholder-transparent
-                border-opacity-50 outline-none hover:border-[#2e8a99] focus:border-[#2e8a99] focus:text-black transition duration-300 -z-10' 
-                placeholder={labelName} />     
-                <label htmlFor='input' className='absolute px-1 mt-1 left-3 peer-placeholder-shown:text-base
-                peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-1.5 transition-all peer-focus:-top-4 peer-focus:left-3 
-                peer-focus:text-[#2e8a99] peer-focus:font-medium peer-focus:bg-white peer-focus:scale-75'>
-                    {labelName}
-                </label>      
-            </div>   
+            <div className='relative w-full'>
+                <input
+                    type={type}
+                    id={name}
+                    name={name}
+                    placeholder=''
+                    className='peer h-12 w-full min-w-[200px] border-2 border-gray-200 text-gray-800 focus:outline-none focus:border-[#2e8a99]/70 
+                    rounded-md pl-3 hover:border-[#2e8a99]/70 placeholder-transparent'
+                    onChange={onChange}
+                    onBlur={handleFocus}
+                    onFocus={() => 
+                        name === 'confirmPassword' && setFocused(true)
+                    }
+                    focused={focused.toString()}
+                    {...inputProps}
+                    required
+                />
+                <label htmlFor={name} className='absolute left-0 -top-2 px-1 bg-white ml-4 text-gray-400 text-xs peer-placeholder-shown:text-base 
+                peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-focus:-top-2 peer-focus:text-[#2e8a99] peer-focus:text-xs transform duration-300'>
+                    {label}
+                </label>
+                <span className='error-msg text-xs text-red-500 leading-tight line-clamp-3 hidden'>
+                    {errorMessage}                                    
+                </span>
+            </div>
         </div>
     )
 }
