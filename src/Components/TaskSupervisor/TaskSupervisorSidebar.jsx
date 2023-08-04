09f0dd7095a924f2688eac57a0e65b736f2451d3
SuraniDS <suranisooriyaarachchi99429@gmail.com>
2023-08-04 05:42:47 +0530

    Create sidebar for task supervisor

---
+++ src/Components/TaskSupervisor/TaskSupervisorSidebar.jsx
@@ -0,0 +1,99 @@
+import logo from '../../Assets/logo.png';
+import React, {useState} from "react";
+import {BsArrowLeftShort} from "react-icons/bs";
+import {FiChevronDown} from "react-icons/fi";
+import dashboardIcon from "../../Assets/Icons/dashboard-icon.png";
+import propertiesIcon from "../../Assets/Icons/properties-icon.png";
+import tasksIcon from "../../Assets/Icons/upcoming-tasks-icon.png";
+import taskApprovalsIcon from "../../Assets/Icons/task-approvals-icon.png";
+// import Sidebar from "../Common/Sidebar";
+
+const TaskSupervisorSidebar = () => {
+
+    const[open , setOpen] = useState(true);
+    const [openSubmenus, setOpenSubmenus] = useState([]);
+
+    const SidebarItems = [
+        {title: "Dashboard",
+            icon: dashboardIcon},
+        {title: "Properties",
+            icon: propertiesIcon,
+            submenu: true,
+            submenuItems: [
+                {title: "Assigned Properties"},
+                {title: "Properties to be Managed"},
+            ]},
+        {title: "Tasks",
+            icon: tasksIcon,
+            submenu: true,
+            submenuItems: [
+                {title: "Upcoming Tasks"},
+                {title: "Ongoing Tasks"},
+                {title: "Completed Tasks"},
+            ]},
+        {title: "Task Approvals",
+            icon: taskApprovalsIcon},
+    ]
+
+       return (
+           <div className='flex'>
+               <section className={`sidebar h-screen bg-gray-100 text-primary-blue-800 pt-2 relative duration-300
+                   ${open ? 'w-80' : 'w-28'}`}>
+                   <BsArrowLeftShort className={`absolute bg-white text-3xl text-primary-blue-800
+                       rounded-full border-2 border-primary-blue-800 cursor-pointer -right-3 top-6
+                       ${!open && 'rotate-180'}`}
+                       onClick={() => setOpen(!open)}/>
+                   <div className={`row-auto flex space-x-6 ${!open ? 'pl-4' : 'pl-6'}`}>
+                       <img className={`${!open ? 'w-20 h-20' : 'w-20 h-20'} duration-300`} src={logo} alt='logo'/>
+                       <div className='text-center pt-1'>
+                           <h1 className={`title text-3xl p-0 ${!open && 'scale-0'} duration-300`}>Property<br/> Pulse</h1>
+                       </div>
+                   </div>
+
+                   <ul className='pt-6'>
+                       {SidebarItems.map((menu, index) => (
+                           <React.Fragment key={index}>
+                               <li className='flex items-center text-primary-blue-800 px-7 py-3 m-2 my-3 gap-x-5 cursor-pointer
+                               hover:bg-selected rounded-xl my-2'>
+                                   <span className='text-4xl block float-left'>
+                                       <img className='w-8 h-8' src={menu.icon} alt='logo'/>
+                                   </span>
+
+                                   <span className={`pt-1 font-semibold text-lg flex-1 ${!open && "hidden"}`}>
+                                       {menu.title}
+                                   </span>
+
+                                   {menu.submenu && open && (
+                                        <FiChevronDown className={`pt-2 text-3xl ${openSubmenus.includes(index) && "rotate-180"}`}
+                                            onClick={() => {
+                                                setOpenSubmenus(prevState => {
+                                                    if (prevState.includes(index)) {
+                                                        return prevState.filter(item => item !== index);
+                                                    } else {
+                                                        return [...prevState, index];
+                                                    }
+                                                });
+                                            }}/>
+                                   )}
+                               </li>
+                               {menu.submenu && openSubmenus.includes(index)  && open && (
+                                   <ul>
+                                       {menu.submenuItems.map((submenuItem, index) => (
+                                           <li key={index} className={`text-primary-blue-600 flex items-center font-medium
+                                           cursor-pointer py-3 m-2 hover:bg-selected rounded-lg my-1 pl-20 ${!open && "hidden"}`}>
+                                               {submenuItem.title}
+                                           </li>
+                                       ))}
+                                   </ul>
+                               )}
+                           </React.Fragment>
+                       ))}
+                   </ul>
+
+               </section>
+           </div>
+           // <Sidebar Menus={SidebarItems} />
+    )
+}
+
+export default TaskSupervisorSidebar;
--- src/pages/Task Supervisor/TaskSupervisorDashboard.jsx
+++ src/pages/Task Supervisor/TaskSupervisorDashboard.jsx
@@ -1,8 +1,16 @@
 import NavbarWithSidebar from "../../Components/Common/NavbarWithSidebar";
+import TaskSupervisorSidebar from "../../Components/TaskSupervisor/TaskSupervisorSidebar";
 
 const TaskSupervisorDashboard = () => {
     return(
-        <NavbarWithSidebar name="Deepamal Shaminda" userRole="Task Supervisor" />
+        <div className='w-full flex bg-white'>
+            <div className='z-50'>
+                <TaskSupervisorSidebar />
+            </div>
+            <div className='w-full z-40'>
+                <NavbarWithSidebar name="Deepamal Shaminda" userRole="Task Supervisor" />
+            </div>
+        </div>
     )
 }
 
