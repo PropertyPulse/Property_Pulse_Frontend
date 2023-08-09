5d6eefbc63c30c7dd92b64e3c86d4472fc4c9a78
Deepamalshaminda <deepamalsh@gmail.com>
2023-08-09 11:40:28 +0530

    Create manage to be properties UI for task supervisor.

--- src/pages/Task Supervisor/AssignedProperties.jsx
+++ src/pages/Task Supervisor/AssignedProperties.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import filterIcon from "./../../Assets/Icons/filter-icon.png"
-import sortIcon from "./../../Assets/Icons/sort-icon.png"
+import filterIcon from "../../Assets/Icons/filter-icon.png"
+import sortIcon from "../../Assets/Icons/sort-icon.png"
 import {Button} from "flowbite-react";
 
-const SearchComponent= () => {
+const AssignedProperties= () => {
     return (
         // <form className="max-w-sm px-4">
         //     <div className="relative pl-24">
@@ -40,19 +40,19 @@ const SearchComponent= () => {
                                  className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
                                 <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
                                             out</a>
                                     </li>
@@ -75,19 +75,19 @@ const SearchComponent= () => {
                                  className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
                                 <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
                                     </li>
                                     <li>
-                                        <a href="#"
+                                        <a href="Common#"
                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
                                             out</a>
                                     </li>
@@ -261,4 +261,4 @@ const SearchComponent= () => {
     );
 }
 
-export default SearchComponent;
+export default AssignedProperties;
---
+++ src/pages/Task Supervisor/PropertiesToBeManaged.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ManageProperties from "./ManageProperties";
+
+
+const PropertiesToBeManaged = () => {
+    return (
+        <section>
+            <div className= 'pt-10' >
+                <ManageProperties />
+            </div>
+        </section>
+    )
+}
+
+export default PropertiesToBeManaged
