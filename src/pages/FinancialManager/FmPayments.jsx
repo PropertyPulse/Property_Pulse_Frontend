1a99fd48e654da9a8af607b4ed4f2db944040097
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-08-09 12:57:33 +0530

    updated fm manageer

---
+++ src/pages/FinancialManager/FmPayments.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const FmPayments = () => {
+    return (
+        <div>FmPayments</div>
+    )
+}
+export default FmPayments
---
+++ src/pages/FinancialManager/FmTransactionHistory.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const FmTransactionHistory = () => {
+    return (
+        <div>FmTransactionHistory</div>
+    )
+}
+export default FmTransactionHistory
