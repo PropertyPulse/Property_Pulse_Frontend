01467e7ae781a4c3028716a7d4ce5bbb065f2861
thilankas961 <thilankas961@gmail.com>
2023-08-17 00:32:36 +0530

    finance-manager-added

---
+++ src/Components/InsuranceManager/IsDashboard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../../Components/Common/Navbar";
+import { Outlet } from "react-router-dom";
+import InsuranceSidebar from "../../Components/InsuranceManager/InsuranceSidebar";
+
+const IsDashboard = () => {
+    return (
+        <div className='flex w-full bg-background-blue'>
+            <div className='z-50'>
+                <InsuranceSidebar />
+            </div>
+            <div className='w-full'>
+                <div className='z-40 w-full'>
+                    <Navbar name="Thilanka Jayathilaka" userRole="Insurance Agent" />
+                </div>
+                 <Outlet/>
+                    
+            </div>
+        </div>
+     
+    );
+}
+
+export default IsDashboard;
