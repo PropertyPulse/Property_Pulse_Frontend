670a045c1e269f0d969c820ea6264ab06a8955a0
SuraniDS <suranisooriyaarachchi99429@gmail.com>
2023-09-14 08:16:42 +0530

    Implement frontend for add valuation experts

--- src/pages/System Admin/AddTopManagers.jsx
+++ src/pages/System Admin/AddTopManagers.jsx
@@ -28,11 +28,9 @@ const AddTopManagers = () => {
                 console.log(response.data);
                 navigate(-1); // Navigate back to the previous page
             } else {
-                // Handle unsuccessful response here
                 console.log('Request was not successful:', response);
             }
         } catch (err) {
-            // Handle errors more gracefully here
             console.error('Error:', err);
         }
     };
@@ -47,21 +45,31 @@ const AddTopManagers = () => {
             label: 'User Role',
             pattern: "^[A-Za-z]+$",
             required: true,
-            styles: 'w-2/5 border-0',
+            styles: 'w-1/3 border-0',
             defaultValue: 'Top Manager',
         },
         {
             id: 2,
-            name: 'userName',
+            name: 'firstName',
             type: 'text',
             // errorMessage: "This is a required field",
-            label: 'User Name',
+            label: 'First Name',
+            pattern: "^[A-Za-z]+$",
+            required: true,
+            styles: 'w-1/3 border-0'
+        },
+        {
+            id: 3,
+            name: 'lastName',
+            type: 'text',
+            // errorMessage: "This is a required field",
+            label: 'Last Name',
             pattern: "^[A-Za-z]+$",
             required: true,
             styles: 'w-auto flex-auto border-0'
         },
         {
-            id: 3,
+            id: 4,
             name: 'address',
             type: 'text',
             // errorMessage: "This is a required field",
@@ -70,7 +78,7 @@ const AddTopManagers = () => {
             styles: 'w-3/5 border-0'
         },
         {
-            id: 4,
+            id: 5,
             name: 'district',
             type: 'text',
             // errorMessage: "This is a required field",
@@ -80,8 +88,8 @@ const AddTopManagers = () => {
             styles: 'w-auto flex-auto border-0'
         },
         {
-            id: 5,
-            name: 'contactNumber',
+            id: 6,
+            name: 'contactNo',
             type: 'text',
             // errorMessage: "This is a required field",
             label: 'Contact Number',
@@ -90,7 +98,7 @@ const AddTopManagers = () => {
             styles: 'w-2/5 flex-auto border-0'
         },
         {
-            id: 6,
+            id: 7,
             name: 'email',
             type: 'email',
             // errorMessage: "This is a required field",
@@ -100,7 +108,7 @@ const AddTopManagers = () => {
             styles: 'w-3/5 flex-auto border-0'
         },
         {
-            id: 7,
+            id: 8,
             name: 'nic',
             type: 'text',
             // errorMessage: "Invalid NIC",
@@ -110,7 +118,7 @@ const AddTopManagers = () => {
             styles: 'w-auto flex-auto border-0'
         },
         {
-            id: 8,
+            id: 9,
             name: 'dob',
             type: 'date',
             // errorMessage: "This is a required field and should contain at least 6 characters",
@@ -119,13 +127,13 @@ const AddTopManagers = () => {
             styles: 'w-1/2 border-0'
         },
         {
-            id: 9,
+            id: 10,
             name: 'gender',
             label: 'Gender',
             styles: 'w-1/2 border-0'
         },
         {
-            id: 10,
+            id: 11,
             name: 'password',
             type: 'password',
             // errorMessage: "This is a required field and should contain at least 6 characters",
@@ -175,11 +183,11 @@ const AddTopManagers = () => {
                                             <label className='pl-3 text-md text-secondary-gray'>Gender</label>
                                             <div className='flex justify-around items-center gap-24 text-[#8c8c8c]'>
                                                     <span className='flex items-center'>
-                                                        <input type='radio' className='mx-2' name='gender' id='gender-male' value='Male' defaultChecked onChange={onChange} />
+                                                        <input type='radio' className='mx-2' name='gender' id='gender-male' value='MALE' onChange={onChange} />
                                                         <span className='text-sm'>Male</span>
                                                     </span>
                                                 <span className='flex items-center'>
-                                                        <input type='radio' className='mx-2' name='gender' id='gender-female' value='Female' onChange={onChange} />
+                                                        <input type='radio' className='mx-2' name='gender' id='gender-female' value='FEMALE' onChange={onChange} />
                                                         <span className='text-sm'>Female</span>
                                                     </span>
                                             </div>
